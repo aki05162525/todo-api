2ccf7bead4bdf2c4aa74377fb804f10d6b964bc1
aki05162525 <akihiro05162525@gmail.com>
2025-05-14 00:43:25 +0900

    SampleDTOの作成

---
+++ src/main/java/com/example/todoapi/controller/sample/SampleController.java
@@ -0,0 +1,19 @@
+package com.example.todoapi.controller.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/samples")
+public class SampleController {
+
+    //　GET /samples
+    @GetMapping
+    public SampleDTO index(){
+        return new SampleDTO("OK", LocalDateTime.now());
+    }
+
+}
