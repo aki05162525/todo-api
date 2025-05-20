b07b5872376ff011f2ce0923449f9ea8da4e2843
aki05162525 <akihiro05162525@gmail.com>
2025-05-20 23:57:14 +0900

    mybatisでデータベースにアクセス

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into samples (content) values ( 'hello, world!' );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,3 +2,4 @@ CREATE TABLE samples(
                         id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
                         content VARCHAR(256) NOT NULL
 );
+
